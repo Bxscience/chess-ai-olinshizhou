9e6419acc328fa74deccd7f82b2e9cfde33bc4da
apflour <ericshi700@gmail.com>
2025-02-19 19:13:27 -0500

    chess board script

--- ChessAI/ChessAI/Assets/Scipts/BoardGenerator.cs
+++ ChessAI/ChessAI/Assets/Scipts/BoardGenerator.cs
@@ -6,14 +6,17 @@ public class BoardGenerator : MonoBehaviour
 {
     public const int BoardSize = 8;
     public ChessPiece[,] board = new ChessPiece[BoardSize, BoardSize];
+    public GameObject chessBoard;
     
     void Start()
     {
-        InitalizeBoard();
+        InitializeBoard();
     }
 
     void InitializeBoard()
     {
+        Instantiate(chessBoard, new Vector3(0,0,0), Quaternion.Euler(-90, 0, 0));
+
         // Initialize all squares as empty
         for (int x = 0; x < BoardSize; x++) {
             for (int y = 0; y < BoardSize; y++) {
